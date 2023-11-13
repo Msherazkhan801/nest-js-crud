bc8b54eb9888bd58501ef880d017703849437ce6
sheraz khan <sherazkhan801@gmail.com>
2023-11-13 23:56:13 +0500

    added desc

--- src/todo/todo.entity.ts
+++ src/todo/todo.entity.ts
@@ -10,6 +10,9 @@ export class TodoEntity {
   @Column({ type: 'varchar', length: 100, nullable: false })
   title: string;
 
+  @Column({ type: 'string', length: 100, nullable: false })
+  desc: string;
+
   @Column({ default: false })
   completed: boolean;
 }
